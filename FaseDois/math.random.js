eacbe67e232e8e640640e63e181020b941e3f63a
Rafael Alves <89037281+rafael211999@users.noreply.github.com>
2021-08-18 16:37:18 -0300

    Delete FaseDois directory

--- FaseDois/math.random.js
+++
@@ -1,7 +0,0 @@
-//Para casa.
-//Pesquisar o que significa: 
-//Math.random()
-
-//Ela vai gerar um numero aleatório entre um e zero. Sempre incluindo o 0 e nunca chegando até o 1.
-Math.random();
-console.log("Esse é um exemplo da função Math.random = ",Math.random());
